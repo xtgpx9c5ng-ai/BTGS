288326ae735daa7df70e937fb029e8ce84589bcc
xtgpx9c5ng-ai <xtgpx9c5ng@privaterelay.appleid.com>
2026-09-14 13:58:41 +0200

    Refactor function signatures and includes in pow.h

--- src/pow.h
+++ src/pow.h
@@ -5,46 +5,26 @@
 
 #ifndef BITCOIN_POW_H
 #define BITCOIN_POW_H
-
+#include <arith_uint256.h>
 #include <consensus/params.h>
-
+#include <uint256.h>
 #include <cstdint>
 
 class CBlockHeader;
 class CBlockIndex;
-class uint256;
-class arith_uint256;
 
-/**
- * Convert nBits value to target.
- *
- * @param[in] nBits     compact representation of the target
- * @param[in] pow_limit PoW limit (consensus parameter)
- *
- * @return              the proof-of-work target or nullopt if the nBits value
- *                      is invalid (due to overflow or exceeding pow_limit)
- */
 std::optional<arith_uint256> DeriveTarget(unsigned int nBits, const uint256 pow_limit);
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
+unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params);
+unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
-bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
-bool CheckProofOfWorkImpl(uint256 hash, unsigned int nBits, const Consensus::Params&);
+bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t height, uint32_t old_nbits, uint32_t new_nbits,
+const CBlockIndex* pindexPrev);
 
-/**
- * Return false if the proof-of-work requirement specified by new_nbits at a
- * given height is not possible, given the proof-of-work on the prior block as
- * specified by old_nbits.
- *
- * This function only checks that the new value is within a factor of 4 of the
- * old value for blocks at the difficulty adjustment interval, and otherwise
- * requires the values to be the same.
- *
- * Always returns true on networks where min difficulty blocks are allowed,
- * such as regtest/testnet.
- */
-bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t height, uint32_t old_nbits, uint32_t new_nbits);
+bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params);
+
+std::optional<arith_uint256> DeriveTarget( unsigned int nBits, const uint256 pow_limit);
+bool CheckProofOfWorkImpl(uint256 hash, unsigned int nBits, const Consensus::Params& params);
 
 #endif // BITCOIN_POW_H
